e512afdd8a5bb34891f74608d529e6cf086515c5
Andrius Pilvelis <andrius.pilv@gmail.com>
2020-09-02 15:23:24 +0300

    created add new form + c.r.u.d buttons

--- resources/views/employees.blade.php
+++ resources/views/employees.blade.php
@@ -11,11 +11,20 @@
         <tr>
         <td>{{$value['id']}}</td>
         <td>{{$value['name']}}</td>
+        <td><a href="">Delete</a>
+            <a href="?addEmployee">Add</a>
+            <a href="">Update</a>
+        </td>
         </tr>
         @endforeach
     </table>
 
+@if (isset($_GET['addEmployee']))
+    <form action="" method="post">
+        <div>Name of New employee:</div>
+        <input type="text" name="newName" required><br>
+        <input type="submit" name="addNewEmployee" value="Submit">
+    </form>
+@endif
 
 @endsection
-
-
--- resources/views/projects.blade.php
+++ resources/views/projects.blade.php
@@ -13,9 +13,23 @@
         <td>{{$value['id']}}</td>
         <td>{{$value['project_name']}}</td>
         <td>{{$value['deadline']}}</td>
+        <td><a href="">Delete</a>
+            <a href="?addProject">Add</a>
+            <a href="">Update</a>
+        </td>
         </tr>
         @endforeach
     </table>
+
+    @if (isset($_GET['addProject']))
+    <form action="" method="post">
+        <div>Project name:</div>
+        <input type="text" name="newProjectName" required>
+        <div>Project deadline:</div>
+        <input type="date" name="newProjectDeadline" required><br>
+        <input type="submit" name="addNewProject" value="Submit">
+    </form>
+@endif
 
 
 @endsection
